92e9aeecb267c592bbf55e4ef3e8f98730c43fd0
cretomoharada <k.harada@cretomo.com>
2022-06-19 13:29:38 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import './screens/memo_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.M
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MemoScreen(),
+    );
+  }
+}
---
+++ lib/screens/memo_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MemoScreen extends StatefulWidget {
+  const MemoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MemoScreen> createState() => _MemoScreenState();
+}
+
+class _MemoScreenState extends State<MemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
